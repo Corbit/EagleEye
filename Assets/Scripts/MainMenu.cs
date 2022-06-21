6fe12f394a74c2b2a736a56b8f3a563d19f98d8d
basti <bastian.keck@student.htw-berlin.de>
2022-06-21 15:04:52 +0200

    Level + Pause

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,39 @@ public class MainMenu : MonoBehaviour
     {
       SceneManager.LoadScene("Level1");
     }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
 
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+    public void Level5()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+    public void Level6()
+    {
+        SceneManager.LoadScene("Level6");
+    }
+    public void Level7()
+    {
+        SceneManager.LoadScene("Level7");
+    }
+    public void Level8()
+    {
+        SceneManager.LoadScene("Level8");
+    }
+    public void Level9()
+    {
+        SceneManager.LoadScene("Level9");
+    }
     public void QuitGame()
     {
         Application.Quit();
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool IsPaused;
+    public GameObject PauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsPaused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsPaused = !IsPaused;
+        }
+    }
+
+    public void Resume()
+    {
+        IsPaused = !IsPaused;
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainScreen");
+    }
+}
