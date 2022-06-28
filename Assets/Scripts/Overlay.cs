3a329cfcea8bb802cc65706be1bf3f7bd89364db
basti <bastian.keck@student.htw-berlin.de>
2022-06-28 15:53:43 +0200

    Timer + Munition Count

--- Assets/Scripts/Overlay.cs
+++ Assets/Scripts/Overlay.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Overlay : MonoBehaviour
 {
@@ -9,22 +11,40 @@ public class Overlay : MonoBehaviour
     GameObject[] collectiblearr;
     GameObject go;
     public Transform RemainingTransform;
+    public int ammocount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        collectiblearr = new GameObject[5];
-        for (int i = 0; i < 5; i++)
-        {
-            var position = new Vector3(450 + i * 30, 140, 0);
-            go = (Instantiate(myPrefab, position, Quaternion.identity)).gameObject;
-            collectiblearr[i] = go;
-            collectiblearr[i].transform.parent = RemainingTransform;
-        }
+        ammoprefab();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1") && ammocount < collectiblearr.Length)
+        {
+            Destroy(collectiblearr[ammocount]);
+            Debug.Log("Destroy" + ammocount);
+            ammocount++;
+        }
 
+        if (ammocount >= collectiblearr.Length)
+        {
+            Debug.Log("You Lose");
+        }
+    }
+
+
+    public void ammoprefab()
+    {
+        collectiblearr = new GameObject[5];
+        for (int i = 0; i < 5; i++)
+        {
+            var position = new Vector3(-80 + i * 30, 260, 0);
+            go = (Instantiate(myPrefab, position, Quaternion.identity)).gameObject;
+            collectiblearr[i] = go;
+            collectiblearr[i].transform.SetParent(RemainingTransform, false);
+        }
     }
 }
---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public static TimerController instance;
+    public Text timeCounter;
+
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+    private float elapsedTime;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCounter.text = "Time: 00:00.00";
+        timerGoing = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && timerGoing == false)
+        {
+            BeginTimer();
+        }
+    }
+
+    public void BeginTimer()
+    {
+        timerGoing = true;
+        elapsedTime = 0f;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void EndTimer()
+    {
+        timerGoing = false;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            timeCounter.text = timePlayingStr;
+
+            yield return null;
+        }
+    }
+}
