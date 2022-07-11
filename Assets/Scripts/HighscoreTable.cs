76c9dfe7b8430d011c9fe7d97c7e16fceb774b47
basti <bastian.keck@student.htw-berlin.de>
2022-07-11 21:06:29 +0200

    Highscore/Level + UI Prefabs

--- Assets/Scripts/HighscoreTable.cs
+++ Assets/Scripts/HighscoreTable.cs
@@ -7,9 +7,25 @@ public class HighscoreTable : MonoBehaviour
 {
     TimerController highscore;
     public Text HighscoreLv1;
+    public Text HighscoreLv2;
+    public Text HighscoreLv3;
+    public Text HighscoreLv4;
+    public Text HighscoreLv5;
+    public Text HighscoreLv6;
+    public Text HighscoreLv7;
+    public Text HighscoreLv8;
+    public Text HighscoreLv9;
 
     void Awake()
     {
-        HighscoreLv1.text = PlayerPrefs.GetFloat("HighScore",59).ToString();
+        HighscoreLv1.text = PlayerPrefs.GetFloat("HighScore1", 59).ToString();
+        HighscoreLv2.text = PlayerPrefs.GetFloat("HighScore2", 59).ToString();
+        HighscoreLv3.text = PlayerPrefs.GetFloat("HighScore3", 59).ToString();
+        HighscoreLv4.text = PlayerPrefs.GetFloat("HighScore4", 59).ToString();
+        HighscoreLv5.text = PlayerPrefs.GetFloat("HighScore5", 59).ToString();
+        HighscoreLv6.text = PlayerPrefs.GetFloat("HighScore6", 59).ToString();
+        HighscoreLv7.text = PlayerPrefs.GetFloat("HighScore7", 59).ToString();
+        HighscoreLv8.text = PlayerPrefs.GetFloat("HighScore8", 59).ToString();
+        HighscoreLv9.text = PlayerPrefs.GetFloat("HighScore9", 59).ToString();
     }
 }
--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class TimerController : MonoBehaviour
@@ -33,10 +34,7 @@ public class TimerController : MonoBehaviour
         timeCounter.text = "00,00";
         timerGoing = false;
         BeginTimer();
-        if (Best != null)
-        {
-            Best.text = PlayerPrefs.GetFloat("HighScore", 59).ToString();
-        }
+        gethighscore();
     }
 
     void Update()
@@ -47,13 +45,7 @@ public class TimerController : MonoBehaviour
             EndTimer();
             bestTime = winTime.text;
             floatnumber = float.Parse(bestTime);
-            Debug.Log(bestTime);
-            Debug.Log(floatnumber);
-            if (floatnumber < PlayerPrefs.GetFloat("HighScore", 59))
-            {
-                PlayerPrefs.SetFloat("HighScore", floatnumber);
-                Best.text = floatnumber.ToString();
-            }
+            sethighscore();
             WinScreen.SetActive(true);
             Time.timeScale = 0f;           
         }
@@ -90,6 +82,149 @@ public class TimerController : MonoBehaviour
         if(GUI.Button(new Rect(100, 200, 200,  60), "Delete"))
         {
             PlayerPrefs.DeleteAll();
+        }
+    }
+
+    public void gethighscore()
+    {
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore1", 59).ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level2")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore2", 59).ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level3")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore3", 59).ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level4")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore4", 59).ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level5")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore5", 59).ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level6")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore6", 59).ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level7")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore7", 59).ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level8")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore8", 59).ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level9")
+        {
+            if (Best != null)
+            {
+                Best.text = PlayerPrefs.GetFloat("HighScore9", 59).ToString();
+            }
+        }
+    }
+
+    public void sethighscore()
+    {
+        if(SceneManager.GetActiveScene().name == "Level1")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore1", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore1", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level2")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore2", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore2", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level3")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore3", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore3", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level4")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore4", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore4", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level5")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore5", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore5", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level6")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore6", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore6", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level7")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore7", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore7", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level8")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore8", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore8", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Level9")
+        {
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore9", 59))
+            {
+                PlayerPrefs.SetFloat("HighScore9", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
         }
     }
 }
