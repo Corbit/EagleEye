31b8e8193d0b4381d0178ae074b3c78b0430ef0f
basti <bastian.keck@student.htw-berlin.de>
2022-07-11 16:38:45 +0200

    BestTime

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -8,7 +8,12 @@ public class TimerController : MonoBehaviour
 {
     public static TimerController instance;
     public Text timeCounter;
+    public String bestTime;
     public Text winTime;
+    public Text Best;
+    public float floatnumber = float.Parse("60");
+    public Text Second;
+    public Text Third;
 
     private TimeSpan timePlaying;
     private bool timerGoing;
@@ -17,8 +22,6 @@ public class TimerController : MonoBehaviour
     public GameObject Target;
     public GameObject WinScreen;
 
-    public Text[] winTimeArr1;
-
     private void Awake()
     {
         instance = this;
@@ -26,9 +29,13 @@ public class TimerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timeCounter.text = "Time: 00:00.00";
+        timeCounter.text = "00,00";
         timerGoing = false;
         BeginTimer();
+        if (Best != null)
+        {
+            Best.text = PlayerPrefs.GetFloat("HighScore", 0).ToString();
+        }
     }
 
     void Update()
@@ -37,8 +44,17 @@ public class TimerController : MonoBehaviour
         {
             winTime.text = timeCounter.text;
             EndTimer();
+            bestTime = winTime.text;
+            floatnumber = float.Parse(bestTime);
+            Debug.Log(bestTime);
+            Debug.Log(floatnumber);
+            if (floatnumber < PlayerPrefs.GetFloat("HighScore", 0))
+            {
+                PlayerPrefs.SetFloat("HighScore", floatnumber);
+                Best.text = floatnumber.ToString();
+            }
             WinScreen.SetActive(true);
-            Time.timeScale = 0f;
+            Time.timeScale = 0f;           
         }
     }
 
@@ -61,7 +77,7 @@ public class TimerController : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             timePlaying = TimeSpan.FromSeconds(elapsedTime);
-            string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            string timePlayingStr = "" + timePlaying.ToString("ss','ff");
             timeCounter.text = timePlayingStr;
 
             yield return null;
